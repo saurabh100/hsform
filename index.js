f24d6124a6b83963b3deac7bc34167236a525c71
saurabh upadhyay <saurabh2016@outlook.com>
2019-09-04 00:04:24 +0530

    gfghfh

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cors = require('cors');
 
 var config = {
-    "port": 3000
+    "port": 8080
 };
 
 
@@ -59,6 +59,6 @@ process.on('uncaughtException', function (e) {
 
 app.listen(config.port);
 console.log("Server started at port: ", config.port);
-console.log("browse to URL: https://hsform.herokuapp.com:3000");
+console.log("browse to URL: http://localhost:8080");
 
 module.exports = app;
